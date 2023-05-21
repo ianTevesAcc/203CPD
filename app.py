13635fb6c79cc42dd30769cf2d312900486fac45
nicki-choo <270168718@yoobeestudent.ac.nz>
2023-05-21 19:44:37 +1200

    login API, with some minor edits

--- app.py
+++ app.py
@@ -3,54 +3,62 @@ import sqlite3
 app = Flask(__name__)
 
 
-@app.route('/register', methods=['GET', 'POST'])
-def register_form():
-    if request.method == 'POST':
-        username = request.form['username']
-        email = request.form['email']
-        password = request.form['password']
-
-        conn = sqlite3.connect('users.db')
-        c = conn.cursor()
-
-        # Check if the username already exists
-        c.execute('SELECT * FROM users WHERE username=? OR email=?', (username, email))
-        existing_user = c.fetchone()
-
-
-        if existing_user:
-            error = 'Username already exists.'
-            return render_template('register.html')
-
-        # Insert the new user into the database
-        c.execute('INSERT INTO users (username, email, password) VALUES (?, ?, ?)', (username, email, password))
-        conn.commit()
-        conn.close()
-
-        return redirect('/login')
-
+@app.route('/user_info', methods=['GET'])
+def main():  # put application's code here
     return render_template('register.html')
 
-@app.route('/login', methods=['GET', 'POST'])
+
+@app.route('/register', methods=['POST'])
+def register_user():
+    conn = sqlite3.connect('users.sqlite')
+    cursor = conn.cursor()
+
+    received_data_obj = request.form
+    data_obj_to_save = dict(received_data_obj)
+
+    data_model = {
+        'username': data_obj_to_save['username'],
+        'email': data_obj_to_save['email'],
+        'password': data_obj_to_save['password']
+
+    }
+
+    sql_query = """INSERT INTO users (username, email, password) VALUES (?,?,?)"""
+    cursor.execute(sql_query, (data_model['username'], data_model['email'], data_model['password']))
+
+    conn.commit()
+    return redirect('/login')
+
+
+
+
+@app.route('/login', methods=['GET'])
 def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-
-        conn = sqlite3.connect('users.db')
-        c = conn.cursor()
-
-        # Retrieve the user from the database
-        c.execute('SELECT * FROM users WHERE username=? AND password=?', (username, password))
-        user = c.fetchone()
-
-        if user:
-            return redirect('/')
-        else:
-            error = 'Invalid username or password.'
-            return render_template('login.html')
-
     return render_template('login.html')
+
+
+
+
+
+
+    # if request.method == 'POST':
+    #     username = request.form['username']
+    #     password = request.form['password']
+    #
+    #     conn = sqlite3.connect('users.sqlite')
+    #     c = conn.cursor()
+    #
+    #
+    #     c.execute('SELECT * FROM users WHERE username=? AND password=?', (username, password))
+    #     user = c.fetchone()
+    #
+    #     if user:
+    #         return redirect('/')
+    #     else:
+    #         error = 'Invalid username or password.'
+    #         return render_template('login.html')
+    #
+    # return render_template('login.html')
 
 
 
