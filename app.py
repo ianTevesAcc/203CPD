49ff4cff10af576ee0814145bde80ea0a386e8fa
nicki-choo <270168718@yoobeestudent.ac.nz>
2023-05-23 17:23:06 +1200

    login API, static file rename

--- app.py
+++ app.py
@@ -27,42 +27,16 @@ def register_user():
     cursor.execute(sql_query, (data_model['username'], data_model['email'], data_model['password']))
 
     conn.commit()
+
     return redirect('/login')
-
-
-
 
 @app.route('/login', methods=['GET'])
 def login():
     return render_template('login.html')
 
 
-
-
-
-
-    # if request.method == 'POST':
-    #     username = request.form['username']
-    #     password = request.form['password']
-    #
-    #     conn = sqlite3.connect('users.sqlite')
-    #     c = conn.cursor()
-    #
-    #
-    #     c.execute('SELECT * FROM users WHERE username=? AND password=?', (username, password))
-    #     user = c.fetchone()
-    #
-    #     if user:
-    #         return redirect('/')
-    #     else:
-    #         error = 'Invalid username or password.'
-    #         return render_template('login.html')
-    #
-    # return render_template('login.html')
-
-
-
-
 if __name__ == '__main__':
     app.run(debug=True)
+
+
 
